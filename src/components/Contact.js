b35456baddda6d0a05aa941e08264dcafc7d3b38
hossainrabbi <hossainrabbi259@gmail.com>
2022-11-23 15:19:12 +0600

    email send added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,19 @@
+import { useForm } from '@formspree/react';
+import swal from 'sweetalert';
 import Title from './Title';
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm('mdojvnoy');
+  if (state.succeeded) {
+    swal(
+      'Thanks for sending the message!',
+      'I will reply as soon as possible!',
+      'success'
+    );
+  } else if (state?.errors[0]?.message) {
+    swal('Message Sending Fail!', state?.errors[0]?.message, 'error');
+  }
+
   return (
     <section className="wrapper py-10" id="contact">
       <Title
@@ -9,27 +22,37 @@ const Contact = () => {
         email
       />
 
-      <form className="w-full mx-auto max-w-3xl shadow-[inset_0_0_10px_#000] p-8 rounded-md mt-5">
+      <form
+        className="w-full mx-auto max-w-3xl shadow-[inset_0_0_10px_#000] p-8 rounded-md mt-5"
+        onSubmit={handleSubmit}
+      >
         <input
           className="text-input"
           type="text"
+          name="username"
           placeholder="Enter Your Name"
           required
         />
         <input
           className="text-input  my-3"
           type="email"
+          name="email"
           placeholder="Enter Your Email"
           required
         />
         <textarea
           className="text-input"
+          name="message"
           placeholder="Your Message"
           required
           rows={4}
         />
-        <button type="submit" className="btn-primary w-full mt-3">
-          Send
+        <button
+          type="submit"
+          className="btn-primary w-full mt-3"
+          disabled={state.submitting}
+        >
+          {state.submitting ? 'Sending...' : 'Send'}
         </button>
       </form>
     </section>
