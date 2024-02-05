ee06e9a8216a526431a6b0d7661af23b4d77f9eb
hossainrabbi <hossainrabbi259@gmail.com>
2024-02-05 23:30:23 +0600

    add new resome

---
+++ src/components/Portfolio.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
+import { IoBagHandle } from "react-icons/io5";
+
+export default function Portfolio({
+  name,
+  description,
+  technologies,
+  liveLink,
+  gitHubLink,
+  hideLine,
+}) {
+  return (
+    <article className={`text-white my-8 lg:my-12 relative`}>
+      <div className="absolute -left-10 top-1 bg-primary-bold h-6 w-6 rounded-full flex justify-center items-center ring-offset-2 ring-2 ring-primary-light">
+        <IoBagHandle />
+      </div>
+
+      <div
+        className={`w-0.5 bg-white absolute top-10 ${
+          hideLine ? "h-[calc(100%-2.5rem)]" : "h-full"
+        }`}
+        style={{ left: "-1.82rem" }}
+      />
+
+      <div className="p-8 py-3 lg:p-0">
+        <h4 className="text-2xl">{name}</h4>
+        <p className="text-sm text-gray-400 my-2">{description}</p>
+        <h5 className="text-md mb-1">Technology Used:</h5>
+        <div>
+          {technologies.map((technology) => (
+            <button className="skill-btn" key={technology}>
+              {technology}
+            </button>
+          ))}
+        </div>
+        {liveLink || gitHubLink ? (
+          <div className="flex items-center gap-2.5 mt-5">
+            {liveLink ? (
+              <a
+                className="text-xl hover:text-secondary-light"
+                href={liveLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkAlt />
+              </a>
+            ) : null}
+            {gitHubLink ? (
+              <a
+                className="text-xl hover:text-secondary-light"
+                href={gitHubLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub />
+              </a>
+            ) : null}
+          </div>
+        ) : null}
+      </div>
+    </article>
+  );
+}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,25 @@
 import { projectsData } from "../data/data";
-import Project from "./Project";
+import Portfolio from "./Portfolio";
 import Title from "./Title";
 
 const Projects = () => {
   return (
     <section className="wrapper pt-10" id="projects">
-      <Title
-        title="My Works"
-        description="I enjoy turning complex problems into simple, beautiful, and intuitive interface designs. I develop lots of sites with React JS in front-end and back-end with Node JS, Express JS, and MongoDB. Here are some of my projects:"
-      />
+      <Title title="Projects" description="Here's some recent works of mine" />
 
-      <div>
-        {projectsData.map((project) => (
-          <Project {...project} key={project.id} />
+      {/* I enjoy turning complex problems into simple, beautiful, and intuitive interface designs. I develop lots of sites with React JS in front-end and back-end with Node JS, Express JS, and MongoDB. Here are some of my projects: */}
+
+      <div className="max-w-[900px] mx-auto">
+        {projectsData.map((project, idx) => (
+          <Portfolio
+            {...project}
+            key={project.id}
+            hideLine={projectsData?.length === idx + 1}
+          />
         ))}
+        {/* {projectsData.map((project) => (
+          <Project {...project} key={project.id} />
+        ))} */}
       </div>
     </section>
   );
